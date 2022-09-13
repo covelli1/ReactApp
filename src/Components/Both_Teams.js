34e8e07becb2e335b39eed99ce3df090c5d474b0
Vince Nguyen <vince_nguyen@kingston.com>
2022-09-13 15:36:19 -0700

    fixed champion image names

--- src/Components/Both_Teams.js
+++ src/Components/Both_Teams.js
@@ -15,7 +15,7 @@ function Both_Teams(props) {
             team1 = 
             props.teamDict[x].map((player) =>
                 {
-                    champ = player[1].championName.charAt(0).toUpperCase() + player[1].championName.slice(1).toLowerCase() + '.png'
+                    champ = player[1].championName + '.png'
                     console.log(champ)
 
                     return (
@@ -40,8 +40,8 @@ function Both_Teams(props) {
             team2 = 
             props.teamDict[x].map((player) =>
                 {
-                    champ = player[1].championName.charAt(0).toUpperCase() + player[1].championName.slice(1).toLowerCase() + '.png'
-                    console.log(champ)
+                    champ = player[1].championName + '.png'
+                    {console.log(champ)}
                     return (
                         <div className="" key={player[0]}>
                             <div className="flex flex-row items-center">
